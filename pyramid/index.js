21cdef15c57971a424a59f86eab3e441ba7fcd78
Paris Ward <wardparis.pw@gmail.com>
2018-10-27 15:35:37 -0700

    more algos and testing

--- pyramid/index.js
+++ pyramid/index.js
@@ -13,7 +13,49 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//solution #1
+// function pyramid(n) {
+//     //calculate the mid point of our row
+//     const midpoint = Math.floor((2 * n - 1) / 2);
 
-function pyramid(n) {}
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
 
+//         for (let column = 0; column < 2 * n - 1; column++) {
+//            if (midpoint - row <= column && midpoint + row >= column) {
+//                level += '#';
+//            } else {
+//                level += ' ';
+//            }
+//         }
+//         console.log(level);
+//     }
+// }
+
+//solution #2(recursion)
+const pyramid = (n, row = 0, level = '') => {
+    //if row is equal to n, 
+    // return and stop running pyramid function
+    if( row === n) {
+        return;
+    }
+    //if level is equal to midpoint,
+    // console.log the level and rerun pyramid function
+    if( level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+    
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    //temp variable add will contain the character we need to add to our level string 
+    let add;
+    if( midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#'
+    } else {
+        add = ' ';
+    }
+    //return pyramid function with n, row, and include
+    //the newest character(add) to our level
+    pyramid(n, row, level + add);
+}
 module.exports = pyramid;
--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,33 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// solution #1
+// function vowels(str) {
+//     //define a counter variable starting at 0
+//     let count = 0;
+//     //define a checker array to hold our vowels
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
+//     //iterate through str
+//     for (let char of str.toLowerCase()) {
+//         //if current character includes a vowel(checker)
+//         if(checker.includes(char)) {
+//             //increment by 1
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
+
+// solution #2 (regex)
+const vowels = (str) => {
+    //match the given vowels using regex with two flags g and i,
+    //return will be an array of matches or null if no matches
+    //g makes sure we dont stop at the first match
+    //i(insensitive) makes the match insensitive to capital or lower case
+    const matches = str.match(/[aeiou]/gi)
+    //if an array, return matches.length, if null return 0
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
