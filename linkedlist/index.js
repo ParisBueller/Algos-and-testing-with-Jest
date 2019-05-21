7122f069d9a25c6c62030fb9b10e039b21ff7cfb
Paris Ward <wardparis.pw@gmail.com>
2019-05-21 09:32:29 -0700

    LinkedList insertFirst & size

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -9,6 +9,33 @@ class Node {
     }
 }
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        //create a new node and assign it to the head property,
+        //to make sure we do not overwrite any existing node
+        //we can pass it as the second argument to the new Node().
+        this.head = new Node(data, this.head);     
+    }
+
+    size() {
+        let counter = 0;
+        //get a referennce to our first node in the LinkedList
+        let node = this.head;
+        //while a head node exists
+        while (node) {
+            //immediately increment by one if there is a head
+            counter++;
+            //crawl through the list by calling the next property,
+            //will be either another node or the value null
+            node = node.next;
+        }
+        
+        return counter;
+    }
+}
 
 module.exports = { Node, LinkedList };
